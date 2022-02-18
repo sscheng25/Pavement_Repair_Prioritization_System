890c23ed0b039fd73d9212e48186ee22a5f267c5
jennaepstein <jennaepstein93@gmail.com>
2022-02-17 20:31:16 -0500

    gitignore stuff

--- Jenna.R
+++
@@ -1,9 +0,0 @@
-# Jenna
-
-# Census API key
-census_api_key("41e1c0d912341017fa6f36a5da061d3b23de335e", overwrite = TRUE)
-
-# The "el_paso_pass2" shapefile is too large for our GitHub repo - load in the file locally 
-elpaso_pass <- st_read("Data_JennaLocal/elpaso_pass2/elpaso_pass2.shp") %>%
-  st_transform('ESRI:102339')
-
--- Sisun.R
+++
@@ -1,8 +0,0 @@
-# Sisun
-
-# Census API Key
-census_api_key("e79f3706b6d61249968c6ce88794f6f556e5bf3d", overwrite = TRUE)
-
-# The "el_paso_pass2" shapefile is too large for our GitHub repo - load in the file locally 
-elpaso_pass <- st_read("C:/Users/CSS/Desktop/MUSA801/Data/PCI_Study/elpaso_pass2.shp") %>%
-  st_transform('ESRI:102339')
