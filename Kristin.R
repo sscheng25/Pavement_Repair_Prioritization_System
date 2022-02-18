74f7af64bed7ee39587ecf202568adcc4fa2447b
Kristin Chang <krchang@design.upenn.edu>
2022-02-17 20:07:48 -0500

    RMD updates
    added roadbed base and surface materials data, categorical correlation plots, updated file pathway in my source file

--- Kristin.R
+++ Kristin.R
@@ -4,4 +4,4 @@
 census_api_key("d9eb52b3755db802bb6aa16b739687dde1e42613", overwrite=TRUE)
 
 # The "el_paso_pass2" shapefile is too large for our GitHub repo - load in the file locally 
-elpaso_pass <- st_read("C:/Users/kchan/Desktop/Spring2022/MUSAPracticum/EPrepo/driveData/elpaso_pass2/elpaso_pass2.shp")
+elpaso_pass <- st_read("C:/Users/kchan/Desktop/Spring2022/MUSAPracticum/driveData/elpaso_pass2/elpaso_pass2.shp")
